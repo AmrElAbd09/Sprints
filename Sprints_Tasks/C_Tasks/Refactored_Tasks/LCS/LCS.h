3b0bcbfec0429eb46b8517e0f478fc39bd9150c4
AmrElAbd09 <128422619+AmrElAbd09@users.noreply.github.com>
2023-04-02 05:39:50 +0200

    Create LCS.h

---
+++ Sprints_Tasks/C_Tasks/Refactored_Tasks/LCS/LCS.h
@@ -0,0 +1,13 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+int8_t insertionSort(int32_t* array, uint8_t arraySize);
+void printArray(int32_t* array, uint8_t arraySize);
+int8_t lcsGetSize(int32_t* array, uint8_t arraySize, uint8_t* sizeofLCS);
+
