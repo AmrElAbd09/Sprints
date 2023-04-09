9e00365eb5ef16dee9dc8f5315045b6979d60a97
AmrElAbd09 <128422619+AmrElAbd09@users.noreply.github.com>
2023-04-09 17:31:42 +0200

    Update App.c

--- Sprints_Tasks/Embedded_Level_1/LED_V1/Application/App.c
+++ Sprints_Tasks/Embedded_Level_1/LED_V1/Application/App.c
@@ -2,93 +2,79 @@
  * app.c
  *
  * Created: 06-Apr-23 3:02:19 AM
- *  Author: Amr
+ *  Author: Zerconium
  */ 
 
 /*****************************************************************************************************************************************************************
 *																		Includes																				 *
 ******************************************************************************************************************************************************************/
 #include "app.h"
-/*****************************************************************************************************************************************************************
-*																		Global variables																				 *
-******************************************************************************************************************************************************************/
-volatile uint8_t LED_Number =0;
+#include "util/delay.h"
 /*****************************************************************************************************************************************************************
 *																		Functions definitions 																	 *
 ******************************************************************************************************************************************************************/
-static void press (void){
-	if(LED_Number < 7){
-		LED_Number++;
-	}
-	else{
-		LED_Number = 0 ;
-	}
-}
-
 void APP_init()
 {
 	LED_init(porta, pin0);
 	LED_init(porta, pin1);
 	LED_init(porta, pin2);
 	LED_init(porta, pin3);
-
-	button_init(portd, pin2);
-
-
-sei();
-EXI_Enable (EXT_INT_0);
-EXI_Trigger(EXT_INT_0,RISING_EDGE);
-EXI_SetCallBack(EXT_INT_0,press);
-
-
-
+	button_init(portc, pin0);
 
 }
 void APP_start()
 {
-
-	while (1)
-	{
-		switch (LED_Number){
-			case 0 :
-			LED_off(porta, pin0);
-			LED_off(porta, pin1);
-			LED_off(porta, pin2);
-			LED_off(porta, pin3);
-
-			break;
+	EN_state buttonSTATE;
+	uint8_t counter=0;
+	while(1){
+		button_read(portc,pin0,&buttonSTATE);
+		if(buttonSTATE == HIGH)
+		{
 			
-			case 1 :
-			LED_on(porta, pin0);
-			break;
-			
-			case 2 :
-			LED_on(porta, pin1);
-			break;
-			
-			case 3 :
-			LED_on(porta, pin2);
-			break;
-			
-			case 4 :
-			LED_on(porta, pin3);
-			break;
-			
-			case 5 :
-			LED_off(porta, pin0);
-			break;
-			
-			case 6 :
-			LED_off(porta, pin1);
-			break;
-			
-			case 7 :
-			LED_off(porta, pin2);
-			break;
-			
-			default:
-			break;
+			if(counter ==0){
+				LED_on(porta,pin0);
+				_delay_ms(300);
+				counter++;
+			}
+			else if(counter ==1){
+				LED_on(porta,pin1);
+				_delay_ms(300);
+				counter++;
+			}
+			else if(counter ==2){
+				LED_on(porta,pin2);
+				_delay_ms(300);
+				counter++;
+			}
+			else if(counter ==3){
+				LED_on(porta,pin3);
+				_delay_ms(300);
+				counter++;
+			}
+			else if(counter ==4){
+				LED_off(porta,pin0);
+				_delay_ms(300);
+				counter++;
+			}
+			else if(counter ==5){
+				LED_off(porta,pin1);
+				_delay_ms(300);
+				counter++;
+			}
+			else if(counter ==6){
+				LED_off(porta,pin2);
+				_delay_ms(300);
+				counter++;
+			}
+			else if(counter ==7){
+				LED_off(porta,pin3);
+				_delay_ms(300);
+				counter =0;
+			}
 		}
+		
+	
+		
 	}
 }
 
